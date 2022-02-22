6c1c7a540205cb5d1db56bab3baac787e1094990
Yacine GACI <ey_gaci@esi.dz>
2022-02-22 19:51:48 +0100

    Adding template transformer classes

---
+++ src/transforms.py
@@ -0,0 +1,37 @@
+class TemplateTransformer:
+    def __init__(self, group_token="<Group>"):
+        self.group_token = group_token
+
+    def transform(self, input):
+        raise NotImplementedError
+
+
+
+
+class SingleInputSequenceClassificationTemplateTransformer(TemplateTransformer):
+    def transform(self, input):
+        return self.group_token + " " + input
+
+
+
+
+
+class DoubleInputSequenceClassificationTemplateTransformer(TemplateTransformer):
+    def transform(self, input):
+        return (
+            "They " + input,
+            self.group_token + " " + input
+        )
+
+
+
+
+
+
+class QuestionAnsweringTemplateTransformer(TemplateTransformer):
+    def transform(self, input):
+        return (
+            "There are " + self.group_token + ".",
+            "Who " + input + "?"
+        )
+
