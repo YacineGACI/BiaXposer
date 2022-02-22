e63dd76ac9d3a2f3e72ea522ab9bb2a2edf3de73
Yacine GACI <ey_gaci@esi.dz>
2022-02-22 19:17:10 +0100

    Chnaging the InputProcessor classes such that they expect the tokenizers and not tokenizer names

--- inputs.py
+++ inputs.py
@@ -1,17 +1,10 @@
-from transformers import AutoTokenizer
-
-
-
-
-
 class InputProcessor:
     """
         Tokenizes the Input according to the task model.
         Returns a dictionary of input_ids, attention_masks and token_type_ids
     """
-    def __init__(self, tokenizer_name, truncation=True):
-        self.tokenizer_name = tokenizer_name
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    def __init__(self, tokenizer, truncation=True):
+        self.tokenizer = tokenizer
         self.truncarion = truncation
 
 
@@ -28,8 +21,8 @@ class SingleInputProcessor(InputProcessor):
     """
         Tokenizes inputs consisting of a single sentence. suitable for tasks such as Sentiment classification, hate speech detection, correct grammar detection...
     """
-    def __init__(self, tokenizer_name, truncation=True):
-        super().__init__(tokenizer_name, truncation)
+    def __init__(self, tokenizer, truncation=True):
+        super().__init__(tokenizer, truncation)
 
     def tokenize(self, input):
         return self.tokenizer(input, truncation=self.truncation)
@@ -45,8 +38,8 @@ class DoubleInputProcessor(InputProcessor):
     """
         Tokenizes inputs consisting of two sentences, e.g. Textual Entailment, Paraphrase Detection, Question Answering...
     """
-    def __init__(self, tokenizer_name, truncation=True):
-        super().__init__(tokenizer_name, truncation)
+    def __init__(self, tokenizer, truncation=True):
+        super().__init__(tokenizer, truncation)
 
     def tokenize(self, input):
         input_1 = input[0]
@@ -64,8 +57,8 @@ class MultipleChoiceInputProcessor(InputProcessor):
     """
         Toeknizes inputs consisting of a context, and multiple choices.
     """
-    def __init__(self, tokenizer_name, truncation=True):
-        super().__init__(tokenizer_name, truncation)
+    def __init__(self, tokenizer, truncation=True):
+        super().__init__(tokenizer, truncation)
 
     def tokenize(self, input):
         input_1 = input[0]
