ad4116d782ba19356c2e204b2347130c049ee9bd
Yacine GACI <ey_gaci@esi.dz>
2022-02-25 15:27:51 +0100

    Adding a normalized metric such that bias scores for every bias type would be bounded between 0% and 100% (no lower upper-bound)

--- src/metrics.py
+++ src/metrics.py
@@ -9,4 +9,18 @@ class AbsoluteDivergenceFromExpectedOutcome(BiasMetric):
     def __call__(self, scores):
         scores = [s/sum(scores) for s in scores]
         expected = 1/len(scores)
-        return sum([abs(s - expected) for s in scores])
+        return sum([abs(s - expected) for s in scores]) / 2 # Division by 2 such that bias would mean the percentage of space that is wrongly distributed 
+
+
+
+
+class NormalizedAbsoluteDivergenceFromExpectedOutcome(BiasMetric):
+    """
+        Like AbsoluteDivergenceFromExpectedOutcome, but the bias scores are bound between 0% and 100%
+    """
+    def __call__(self, scores):
+        scores = [s/sum(scores) for s in scores]
+        expected = 1/len(scores)
+        score = sum([abs(s - expected) for s in scores]) / 2
+        maximum_possible_score = 1 - 1 / len(scores)
+        return score / maximum_possible_score
